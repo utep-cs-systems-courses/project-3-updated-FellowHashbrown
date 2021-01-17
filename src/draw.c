5d27bff603ba91aa4db7a5c6b2a83cd9436eb40c
FellowHashbrown <fellowhashbrown@gmail.com>
2021-01-16 23:44:21 -0700

    Made changes to drawSquare function

--- src/draw.c
+++ src/draw.c
@@ -1,14 +1,17 @@
+#include <msp430.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
 #include "draw.h"
 
-void drawSquare(int centerX, int centerY, int width, int height, int color)
+void drawSquare(u_char centerX, u_char centerY, u_char width, u_char height, u_int color)
 {
-  int leftX = centerX - width / 2;
-  int topY  = centerY - height / 2;
-  for (int r = 0; r < width; r++)
-    for (int c = 0; c < height; c++)
+  u_char leftX = centerX - width / 2;
+  u_char topY  = centerY - height / 2;
+  for (u_char r = 0; r < width; r++) {
+    for (u_char c = 0; c < height; c++) {
       drawPixel(topY + c, leftX + r, color);
+    }
+  }
 }
 
 void drawChar8x12(u_char rcol, u_char rrow, char c, u_int fgColorBGR, u_int bgColorBGR)
--- src/draw.h
+++ src/draw.h
@@ -3,7 +3,7 @@
 
 void drawString8x12(u_char col, u_char row, char *string, u_int fgColorBGR, u_int bgColorBGR);
 void drawChar8x12(u_char rcol, u_char rrow, char c, u_int fgColorBGR, u_int bgColorBGR);
-void drawSquare(int centerX, int centerY, int width, int height, int color);
+void drawSquare(u_char centerX, u_char centerY, u_char width, u_char height, u_int color);
 void centerString8x12(u_char row, char *string, u_int fgColorBGR, u_int bgColorBGR);
 
 #endif
